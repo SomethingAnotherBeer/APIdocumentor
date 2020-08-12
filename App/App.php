20a9c047398f0baf27d068279c1b744b02e07fb5
Eugene Zarianski <eugene@devanother.com>
2020-08-12 12:10:16 +0300

    Подготовка основных файлов

--- App/App.php
+++ App/App.php
@@ -7,7 +7,7 @@ class App
 		try{
 			$route = new Services\Route();
 			$controller_params = $route->getRoute($_SERVER['REQUEST_URI']);
-			Includes\DB::setConnection('localhost','test11','root','12345qqqwww');
+			Includes\DB::setConnection('localhost','your_db_name','root','your_password');
 			$controller = new $controller_params[0];
 			$controller->{$controller_params[1]}();
 
